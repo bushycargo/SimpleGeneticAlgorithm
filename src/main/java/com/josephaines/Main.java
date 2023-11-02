c59b837ff52a73f0e5f69fbeca615756e44f642f
bushycargo <josephaines03@gmail.com>
2023-11-02 15:26:47 +0000

    Now tries to run the SGA

--- src/main/java/com/josephaines/Main.java
+++ src/main/java/com/josephaines/Main.java
@@ -1,7 +1,25 @@
 package com.josephaines;
 
+import com.josephaines.sga.Data;
+import com.josephaines.sga.SimpleGeneticAlgorithm;
+import com.josephaines.sga.individual.Algorithm;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int populationSize = 10;
+        int geneLength = 10;
+        int cycles = 10;
+        float mutationProbability = (float) 1 /populationSize;
+        float mutationStep = 0.05f;
+        float min = -5;
+        float max = 5;
+
+        SimpleGeneticAlgorithm simpleGeneticAlgorithm = new SimpleGeneticAlgorithm(populationSize, geneLength,
+                cycles, mutationProbability, mutationStep, min, max, Algorithm.ONE);
+
+        Data data = simpleGeneticAlgorithm.runAlgorithm();
+        System.out.println(Arrays.toString(data.averageUtility));
     }
 }
